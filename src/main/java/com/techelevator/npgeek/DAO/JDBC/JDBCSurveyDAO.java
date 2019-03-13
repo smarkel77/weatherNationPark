ae1352ed30c0daca9bb69edfa78e297c2d1162f0
Joseph Ambrose <josephambrosej@gmail.com>
2019-03-13 11:05:52 -0400

    finished_parkdao

--- src/main/java/com/techelevator/npgeek/DAO/JDBC/JDBCSurveyDAO.java
+++ src/main/java/com/techelevator/npgeek/DAO/JDBC/JDBCSurveyDAO.java
@@ -1,5 +1,22 @@
 package com.techelevator.npgeek.DAO.JDBC;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JDBCSurveyDAO {
 
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public JDBCSurveyDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		
+	}
+	
+	@Override
+	public List<Survey>
 }
