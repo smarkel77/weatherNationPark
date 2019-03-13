13d648e60bc224a02543c48159e249b154005f01
Joseph Ambrose <josephambrosej@gmail.com>
2019-03-13 11:57:48 -0400

    partial_surveyDAO

--- src/main/java/com/techelevator/npgeek/DAO/JDBC/JDBCSurveyDAO.java
+++ src/main/java/com/techelevator/npgeek/DAO/JDBC/JDBCSurveyDAO.java
@@ -1,13 +1,20 @@
 package com.techelevator.npgeek.DAO.JDBC;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import com.techelevator.npgeek.Survey;
+import com.techelevator.npgeek.DAO.SurveyDAO;
+
 @Component
-public class JDBCSurveyDAO {
+public class JDBCSurveyDAO implements SurveyDAO {
 
 	private JdbcTemplate jdbcTemplate;
 	
@@ -18,5 +25,46 @@ public class JDBCSurveyDAO {
 	}
 	
 	@Override
-	public List<Survey>
+	public List<Survey> getAllSurveys(){
+		String sql ="SELECT surveyid, parkcode, emailaddress, state, "
+				+ "activitylevel  FROM survey_result";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		
+		List<Survey> newSurveyForm = new ArrayList<Survey>();
+		
+		while(results.next()){
+			newSurveyForm.add(mapRowToSurvey(results));
+		}
+		return newSurveyForm;
+	}
+	
+	private Survey mapRowToSurvey(SqlRowSet row) {
+		Long surveyId = row.getLong("surveyid");
+		String parkCode = row.getString("parkcode");
+		String emailAddress =row.getString("emailaddress");
+		String state = row.getString("state");
+		String activityLevel =row.getNString("activitylevel");
+		
+		Survey newSurvey = new Survey();
+		
+		newSurvey.setSurveyID(surveyId);
+		newSurvey.setParkCode(parkCode);
+		newSurvey.setEmail(emailAddress);
+		newSurvey.setState(state);
+		newSurvey.setActivityLevel(activityLevel);
+		
+		return newSurvey;
+	}
+
+	@Override
+	public List<Survey> getAllSurveyForms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(Survey newSurveyForm) {
+		// TODO Auto-generated method stub
+		
+	}
 }
