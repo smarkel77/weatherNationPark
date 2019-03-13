53055d9d7c5c7bde0aa984f81b7c6aa5a03922da
Steven Markel <smarkel@gmail.com>
2019-03-13 11:15:36 -0400

    working on jsps

--- src/main/java/com/techelevator/npgeek/Controllers/SurveyController.java
+++ src/main/java/com/techelevator/npgeek/Controllers/SurveyController.java
@@ -1,14 +1,36 @@
 package com.techelevator.npgeek.Controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.techelevator.npgeek.Survey;
 
 @Controller
 public class SurveyController {
 
-	@RequestMapping("/survey")
-	public String displaySurvey() {
+	@RequestMapping(path="/survey", method=RequestMethod.GET)
+	public String displaySurvey(ModelMap modelHolder) {
+		if(! modelHolder.containsAttribute("survey")) {
+			modelHolder.put("survey", new Survey());
+		}
 		return "survey";
 	}
 
+	@RequestMapping(path="survey", method=RequestMethod.POST)
+	public String processSurvey(@Valid @ModelAttribute Survey survey, BindingResult result,
+			RedirectAttributes flash) {
+		flash.addFlashAttribute("survey", survey);
+		if(result.hasErrors()) {
+			flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "survey", result);
+			return "redirect:/survey";
+		}
+		return "redirect:surveyResults";
+	}
 }
--- src/main/webapp/WEB-INF/jsp/homepage.jsp
+++ src/main/webapp/WEB-INF/jsp/homepage.jsp
@@ -1,8 +1,17 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <c:import url="/WEB-INF/jsp/common/header.jsp" />
 
-<h1>survey</h1>
+<h1>Homepage</h1>
+
+<div id="park">
+	<c:forEach var="park" items="${parks}">
+		<img src="<c:url value="img/${park.parkCode}"/>" />
+		<h2>${park.parkName} located in ${park.state}</h2>
+		<p>${park.description}</p>
+	</c:forEach>
+</div>
+
 
 
 <c:import url="/WEB-INF/jsp/common/footer.jsp" />
